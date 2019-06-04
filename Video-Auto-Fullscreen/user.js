de8a12990425d3400fd958515a689f3aff8c74bb
tmr <myemail-tmr@protonmail.com>
2019-06-04 11:41:00 +0800

    fix bilibili user.js in background tab

--- Video-Auto-Fullscreen/user.js
+++ Video-Auto-Fullscreen/user.js
@@ -2,7 +2,7 @@
 // @name         自动网页全屏播放
 // @namespace    https://tmr.js.org/
 // @more         https://github.com/ttttmr/UserJS
-// @version      0.1
+// @version      0.2
 // @description  自动网页全屏播放，已支持Bilibili
 // @author       tmr
 // @match        https://www.bilibili.com/video/av*
@@ -12,30 +12,47 @@
 (function () {
     'use strict';
     let counter = 0;
-    window.addEventListener('load', function () {
+    function fullscreen() {
         console.log('web fullscreen start');
+        webfull()
         function webfull() {
             console.log('web fullscreen ing ' + counter);
             counter++;
-            let ele_class;
+            let play_class;
+            let fullscreen_class;
             if (location.host == 'www.bilibili.com') {
-                ele_class = '.bilibili-player-video-web-fullscreen';
+                fullscreen_class = '.bilibili-player-video-web-fullscreen';
             }
-            if (ele_class) {
-                if (document.querySelector(ele_class)) {
-                    $(ele_class).click();
+            if (fullscreen_class) {
+                // 尝试全屏
+                if (document.querySelector(fullscreen_class)) {
+                    // 网页全屏
+                    $(fullscreen_class).click();
                     console.log('web fullscreen success');
+                    document.removeEventListener('visibilitychange',fullscreen);
                 }
+                // 失败并重试
                 else {
+                    // 超过30次就退出
                     if (counter > 30) {
                         console.log('web fullscreen fail');
                         return;
                     };
+                    // 延迟0.5秒重试
                     setTimeout(webfull, 500);
                 }
             }
         }
-        webfull();
-        console.log('web fullscreen done');
+    }
+    window.addEventListener('load', function () {
+        // 判断后台打开
+        if(document.visibilityState=='hidden') {
+            console.log("now hidden, wait visible");
+            document.addEventListener('visibilitychange',fullscreen);
+        }
+        // 前台打开，直接直行
+        else{
+            fullscreen();
+        }
     });
 })();
