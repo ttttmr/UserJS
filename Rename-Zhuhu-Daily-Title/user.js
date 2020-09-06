7212275d87e10710a698707679e6f7f1d0ae16e9
ttttmr <myemail-tmr@protonmail.com>
2020-09-06 11:54:21 +0800

    update zhihu daily

--- Rename-Zhuhu-Daily-Title/user.js
+++ Rename-Zhuhu-Daily-Title/user.js
@@ -2,7 +2,7 @@
 // @name         修改知乎日报标题
 // @namespace    https://tmr.js.org/
 // @more         https://github.com/ttttmr/UserJS
-// @version      0.3
+// @version      0.4
 // @description  修改知乎日报标题为文章标题
 // @author       tmr
 // @match        *://daily.zhihu.com/story/*
@@ -14,16 +14,11 @@
     function rename() {
         let zh_title = document.querySelector("p[class=DailyHeader-title]").innerText;
         document.querySelector("title").innerText = zh_title + "-知乎日报";
+        document.addEventListener('visibilitychange', () => setTimeout(rename, 500));
     }
-    window.addEventListener('load', function () {
-        // 判断后台打开
-        if (document.visibilityState == 'hidden') {
-            console.log("now hidden, wait visible");
-            document.addEventListener('visibilitychange', rename);
-        }
-        // 前台打开，直接执行
-        else {
-            rename();
-        }
-    });
+    if (document.readyState == 'loading') {
+        document.addEventListener('DOMContentLoaded', rename);
+    } else {
+        rename();
+    }
 })();
