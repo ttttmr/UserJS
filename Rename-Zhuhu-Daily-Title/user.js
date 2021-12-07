812d20d2ee032a11e1db5cd2fed113906b61393c
ttttmr <myemail-tmr@protonmail.com>
2021-12-07 21:59:28 +0800

    fix: zhihu daily

--- Rename-Zhuhu-Daily-Title/user.js
+++ Rename-Zhuhu-Daily-Title/user.js
@@ -2,7 +2,7 @@
 // @name         修改知乎日报标题
 // @namespace    https://blog.xlab.app/
 // @more         https://github.com/ttttmr/UserJS
-// @version      0.5
+// @version      0.6
 // @description  修改知乎日报标题为文章标题
 // @author       tmr
 // @match        *://daily.zhihu.com/story/*
@@ -10,18 +10,14 @@
 // ==/UserScript==
 
 (function () {
-  'use strict';
-  function rename() {
-    let zh_title = document.querySelector(
-      'p[class=DailyHeader-title]'
-    ).innerText;
-    document.querySelector('title').innerText = zh_title + '-知乎日报';
-    setTimeout(rename, 1000);
-  }
-  rename();
-  if (document.readyState == 'loading') {
-    document.addEventListener('onload', () => setTimeout(rename, 1000));
-  } else {
-    setTimeout(rename, 1000);
-  }
+    'use strict';
+    function rename() {
+        let zh_title = document.querySelector("p[class=DailyHeader-title]").innerText;
+        let new_title = zh_title + "-知乎日报";
+        if (document.title !== new_title) {
+            document.title = new_title;
+        }
+        setTimeout(rename, 1000);
+    }
+    rename();
 })();
